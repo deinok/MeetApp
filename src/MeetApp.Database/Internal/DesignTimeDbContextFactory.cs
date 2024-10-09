e3210469a86a2294527308a46071518ed28bbe39
Raul Hidalgo Caballero <deinok@deinok.com>
2024-10-09 14:24:19 +0200

    .

---
+++ src/MeetApp.Database/Internal/DesignTimeDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace MeetApp.Database.Internal
+{
+
+    internal class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var dbContextOptionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            dbContextOptionsBuilder.UseNpgsql();
+            return new AppDbContext(dbContextOptionsBuilder.Options);
+        }
+
+    }
+
+}
