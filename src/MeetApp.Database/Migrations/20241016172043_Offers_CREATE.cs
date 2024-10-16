9e15c506ad3d3a82992db482ad99def1603a8cdd
Tarik Kreso <45802412+tarikkreso@users.noreply.github.com>
2024-10-16 19:27:01 +0200

    Add Offer entity and related database schema changes (#44)
    - Added DbSet<Offer> to AppDbContext for managing Offer entities.
    - Created migration 20241016172043_Offers_CREATE for Offers table.
    - Defined Offer entity with properties: Id, BusinessId, Title, Description, ExpirationDate, and Tag.
    - Established foreign key relationship between Offer and User entities.
    - Updated User entity to include a collection of Offers.
    - Updated AppDbContextModelSnapshot to reflect new Offers table and relationships.

---
+++ src/MeetApp.Database/Migrations/20241016172043_Offers_CREATE.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeetApp.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Offers_CREATE : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Offers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    BusinessId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    Description = table.Column<string>(type: "character varying(1024)", maxLength: 1024, nullable: false),
+                    ExpirationDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    Tag = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Offers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Offers_AspNetUsers_BusinessId",
+                        column: x => x.BusinessId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Offers_BusinessId",
+                table: "Offers",
+                column: "BusinessId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Offers");
+        }
+    }
+}
