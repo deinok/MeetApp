0cfe6cc9371e3ec6535141e107810a7a16e6abfb
José Ramon Ariza Pérez <73638338+jrariza@users.noreply.github.com>
2024-10-20 21:33:31 +0200

    Languages in website #21 (#69)
    * Added english and bosnian languages
    
    * Added english and bosnian languages

--- src/MeetApp.Frontend/src/layout/MainLayout.tsx
+++ src/MeetApp.Frontend/src/layout/MainLayout.tsx
@@ -1,11 +1,19 @@
-import './MainLayoutStyles.css';
-import LogoLogin from '../img/logoWithWhiteLetters.png';
-import React, { useState } from 'react';
-import { Layout, Dropdown, Avatar, Divider, Switch } from 'antd';
-import { useAuthUser, useSignOut } from 'react-auth-kit';
-import { useNavigate } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import i18n from '../i18n';
+import "./MainLayoutStyles.css";
+import LogoLogin from "../img/logoWithWhiteLetters.png";
+import React, { useEffect, useState } from "react";
+import {
+  Layout,
+  Dropdown,
+  Avatar,
+  Divider,
+  Radio,
+  RadioChangeEvent,
+} from "antd";
+import { useAuthUser, useSignOut } from "react-auth-kit";
+import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import i18n from "../i18n";
+import ReactCountryFlag from "react-country-flag";
 
 const { Header, Content, Footer } = Layout;
 
@@ -14,56 +22,74 @@ interface AppLayoutProps {
 }
 
 const AppLayout: React.FC<AppLayoutProps> = ({ children }) => {
-  const { t } = useTranslation('layout');
+  const { t } = useTranslation("layout");
   const auth = useAuthUser();
   const signOut = useSignOut();
   const navigate = useNavigate();
-  const [language, setLanguage] = useState('es');
+  const [language, setLanguage] = useState("es");
 
   const user = auth()?.user;
 
   const handleLogout = () => {
     signOut();
-    navigate('/login');
+    navigate("/login");
   };
 
-  const handleLanguageChange = (checked: boolean) => {
-    const newLang = checked ? 'en' : 'es';
-    setLanguage(newLang);
-    i18n.changeLanguage(newLang);
+  const handleLanguageChange = (e: RadioChangeEvent) => {
+    const lang = e.target.value;
+    setLanguage(lang);
+    localStorage.setItem("language", lang);
+    i18n.changeLanguage(lang);
   };
+
+  // Retrieve language from localStorage when the component mounts
+  useEffect(() => {
+    const savedLanguage = localStorage.getItem("language");
+    if (savedLanguage) {
+      setLanguage(savedLanguage);
+      i18n.changeLanguage(savedLanguage);
+    }
+  }, []);
+
   const handleLogoClick = () => {
-    navigate('/');
+    navigate("/");
   };
 
   const items = [
     {
-      key: 'profile',
-      label: (
-        <a href="/profile">
-          {t('profile')}
-        </a>
-      ),
+      key: "profile",
+      label: <a href="/profile">{t("profile")}</a>,
     },
     {
-      key: 'language',
+      key: "language",
       label: (
         <div>
-          <span>{t('language')}: </span>
-          <Switch
-            checked={language === 'en'}
+          <span>{t("language")}: </span>
+          <Radio.Group value={language} onChange={handleLanguageChange}>
+            <Radio.Button value="es">
+              <ReactCountryFlag countryCode="ES" svg />
+            </Radio.Button>
+            <Radio.Button value="en">
+              <ReactCountryFlag countryCode="US" svg />
+            </Radio.Button>
+            <Radio.Button value="ba">
+              <ReactCountryFlag countryCode="BA" svg />
+            </Radio.Button>
+          </Radio.Group>
+          {/* <Switch
+            checked={language === "en"}
             onChange={handleLanguageChange}
             checkedChildren="EN"
             unCheckedChildren="ES"
-          />
+          /> */}
         </div>
       ),
     },
     {
-      key: 'logout',
+      key: "logout",
       label: (
-        <a onClick={handleLogout}>
-          {t('logout')}
+        <a href="/login" onClick={handleLogout}>
+          {t("logout")}
         </a>
       ),
     },
@@ -73,38 +99,36 @@ const AppLayout: React.FC<AppLayoutProps> = ({ children }) => {
     <Layout className="layout-container">
       <Header className="app-header">
         <div className="header-left">
-        <a onClick={handleLogoClick} className="logo-link">
-          <img
-            src={LogoLogin}
-            alt="Logo"
-            className="logo"
-          />
-          </a>
+          <button onClick={handleLogoClick} className="logo-button">
+            <img src={LogoLogin} alt="Logo" className="logo" />
+          </button>
           <Divider type="vertical" className="custom-divider" />
           <div className="headers-links">
-            <a href="/offers">OFFERS</a>
-            <a href="/stats">STATS</a>
-            </div>
+            <a href="/offers">{t("offers_section")}</a>
+            <a href="/stats">{t("stats_section")}</a>
+          </div>
         </div>
         <div className="header-right">
-          <p className="email-text">{ user?.email || "correo@ejemplo.com"}</p>
+          <p className="email-text">{user?.email || "correo@ejemplo.com"}</p>
           <Divider type="vertical" className="custom-divider" />
-          <Dropdown menu={{ items }} placement="bottomRight" arrow={{ pointAtCenter: true }}>
+          <Dropdown
+            menu={{ items }}
+            placement="bottomRight"
+            arrow={{ pointAtCenter: true }}
+          >
             <Avatar
               src="https://logos-world.net/wp-content/uploads/2020/04/McDonalds-Logo.png"
               size="large"
-              style={{ cursor: 'pointer' }}
+              style={{ cursor: "pointer" }}
             />
           </Dropdown>
         </div>
       </Header>
       <Layout>
-        <Content className="app-content">
-          {children}
-        </Content>
+        <Content className="app-content">{children}</Content>
       </Layout>
       <Footer className="app-footer">
-        <p>© 2024 MeetApp. Todos los derechos reservados.</p>
+        <p>{t("copyright")}</p>
       </Footer>
     </Layout>
   );
