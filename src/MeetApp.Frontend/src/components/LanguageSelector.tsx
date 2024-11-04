bce4009b719d74e4ecb59c3926136639482f2d82
José Ramon Ariza Pérez <joseramonarizaperez@gmail.com>
2024-11-04 21:28:56 +0100

    refactor initial language variable

--- src/MeetApp.Frontend/src/components/LanguageSelector.tsx
+++ src/MeetApp.Frontend/src/components/LanguageSelector.tsx
@@ -2,10 +2,11 @@ import { RadioChangeEvent, Radio } from "antd";
 import React, { useState, useEffect } from "react";
 import ReactCountryFlag from "react-country-flag";
 import { useTranslation } from "react-i18next";
+import { initialLanguage } from "../i18n";
 
 export const LanguageSelector: React.FC = () => {
   const { i18n } = useTranslation();
-  const [language, setLanguage] = useState(localStorage.getItem("language") ?? window.navigator.language.split('-')[0] ?? 'es');
+  const [language, setLanguage] = useState(initialLanguage);
 
   const handleLanguageChange = (e: RadioChangeEvent) => {
     const lang = e.target.value;
