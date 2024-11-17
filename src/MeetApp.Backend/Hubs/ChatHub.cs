db4aa85a63b6e2421736ee5ef17c83ad7695d724
Raul Hidalgo Caballero <deinok@deinok.com>
2024-11-17 14:29:17 +0100

    ref-10 (#135)
    * .
    
    * .

--- src/MeetApp.Backend/Hubs/ChatHub.cs
+++ src/MeetApp.Backend/Hubs/ChatHub.cs
@@ -1,17 +1,22 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using MeetApp.Database;
+using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Threading.Tasks;
 
 namespace MeetApp.Backend.Hubs
 {
 
-    public class ChatHub : Hub<ChatHub.IClient>
+    public class ChatHub(
+        AppDbContext appDbContext
+    ) : Hub<ChatHub.IClient>
     {
+
+        private readonly AppDbContext appDbContext = appDbContext;
 
         public async Task JoinChat(Guid userId, Guid activityId)
         {
-            await Task.CompletedTask;
-            throw new NotImplementedException();
+            await this.Groups.AddToGroupAsync(this.Context.ConnectionId, activityId.ToString());
+            await this.Clients.OthersInGroup(activityId.ToString()).NotifyJoinChat(userId, activityId);
         }
 
         public async Task SendMessage(Guid userId, Guid activityId, string message)
