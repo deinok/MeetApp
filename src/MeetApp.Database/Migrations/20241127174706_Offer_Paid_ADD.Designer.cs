d3378f649c2f2e853f48c2c01a9826ae70dbdc0a
Raul Hidalgo Caballero <rhidalgo@voltec.es>
2024-11-27 18:48:44 +0100

    .

--- src/MeetApp.Database/Migrations/20241127174706_Offer_Paid_ADD.Designer.cs
+++ src/MeetApp.Database/Migrations/20241127174706_Offer_Paid_ADD.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MeetApp.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MeetApp.Database.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241127174706_Offer_Paid_ADD")]
+    partial class Offer_Paid_ADD
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
