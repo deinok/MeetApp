ffc1a929db4b47be76c2334bf07da469ce162c06
Raul Hidalgo Caballero <rhidalgo@voltec.es>
2024-11-27 18:50:56 +0100

    .

--- src/MeetApp.Database/Migrations/20241127174706_Offer_Paid_ADD.cs
+++ src/MeetApp.Database/Migrations/20241127174706_Offer_Paid_ADD.cs
@@ -4,26 +4,26 @@
 
 namespace MeetApp.Database.Migrations
 {
-	/// <inheritdoc />
-	public partial class Offer_Paid_ADD : Migration
-	{
-		/// <inheritdoc />
-		protected override void Up(MigrationBuilder migrationBuilder)
-		{
-			migrationBuilder.AddColumn<bool>(
-				name: "Paid",
-				table: "Offers",
-				type: "boolean",
-				nullable: false,
-				defaultValue: false);
-		}
+    /// <inheritdoc />
+    public partial class Offer_Paid_ADD : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Paid",
+                table: "Offers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
 
-		/// <inheritdoc />
-		protected override void Down(MigrationBuilder migrationBuilder)
-		{
-			migrationBuilder.DropColumn(
-				name: "Paid",
-				table: "Offers");
-		}
-	}
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Paid",
+                table: "Offers");
+        }
+    }
 }
