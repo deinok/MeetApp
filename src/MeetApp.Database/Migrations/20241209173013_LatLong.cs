0b0febd58d9f576b223bbc50a4062acdedc2835b
Raul Hidalgo Caballero <rhidalgo@voltec.es>
2024-12-09 18:34:23 +0100

    .

--- src/MeetApp.Database/Migrations/20241209173013_LatLong.cs
+++ src/MeetApp.Database/Migrations/20241209173013_LatLong.cs
@@ -4,35 +4,35 @@
 
 namespace MeetApp.Database.Migrations
 {
-	/// <inheritdoc />
-	public partial class LatLong : Migration
-	{
-		/// <inheritdoc />
-		protected override void Up(MigrationBuilder migrationBuilder)
-		{
-			migrationBuilder.AddColumn<decimal>(
-				name: "Latitude",
-				table: "AspNetUsers",
-				type: "numeric",
-				nullable: true);
+    /// <inheritdoc />
+    public partial class LatLong : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Latitude",
+                table: "AspNetUsers",
+                type: "numeric",
+                nullable: true);
 
-			migrationBuilder.AddColumn<decimal>(
-				name: "Longitude",
-				table: "AspNetUsers",
-				type: "numeric",
-				nullable: true);
-		}
+            migrationBuilder.AddColumn<decimal>(
+                name: "Longitude",
+                table: "AspNetUsers",
+                type: "numeric",
+                nullable: true);
+        }
 
-		/// <inheritdoc />
-		protected override void Down(MigrationBuilder migrationBuilder)
-		{
-			migrationBuilder.DropColumn(
-				name: "Latitude",
-				table: "AspNetUsers");
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "AspNetUsers");
 
-			migrationBuilder.DropColumn(
-				name: "Longitude",
-				table: "AspNetUsers");
-		}
-	}
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "AspNetUsers");
+        }
+    }
 }
